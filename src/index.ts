71d2f387779f0a713ff69b9bbcc15411e073e085
Frederik Juulsgaard Sørensen <frederikjs1@hotmail.com>
2022-06-16 18:53:56 +0200

    First working release

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export {Form} from "./tools/form-constructor";
 export * from "./tools/form-types";
 export * from "./tools/type-predicates";
 export {formTemplateToControls} from "./tools/templates";
+export {parseFormErrors} from "./tools/errors";
 
 export {FormRootConstructors as FormRoot, ControlFormRoot, ModelFormRoot, AnyControlFormRoot} from "./models/form-root";
 export {FormLayerConstructors as FormLayer, ModelFormLayer, ControlFormLayer, AnyControlFormLayer} from "./models/form-layer";
