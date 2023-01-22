b50a7fac7b034053fdbad8be4c09d073e2c0ee4d
Frederik Sørensen <frederikjs1@hotmail.com>
2023-01-22 18:40:51 +0100

    Remove angular decorator

--- src/form-page/form-confirm.service.ts
+++ src/form-page/form-confirm.service.ts
@@ -1,7 +1,5 @@
-import {Injectable} from "@angular/core";
-
-@Injectable()
 export abstract class FormConfirmService {
+
   abstract confirmSubmit(title: string, text: string, btnText?: string): Promise<boolean>;
 
   abstract confirmDelete(title: string, text: string, btnText?: string): Promise<boolean>;
