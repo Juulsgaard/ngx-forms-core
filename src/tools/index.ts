8df75a700d67f08c279d0bdfd37cb44721a270ca
Frederik Sørensen <frederikjs1@hotmail.com>
2024-04-07 18:57:08 +0200

    Clean up index files

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,10 +1,8 @@
 
 export {FormConstants} from './constants';
 export {formUpdated} from './form-population';
-export {FormValidator, processFormValidators} from './form-validation';
+export {FormValidator, processFormValidators, FormValidationData, FormValidationContext} from './form-validation';
 export {} from './helpers';
 export {} from './templates';
 export * from './type-predicates';
 export {Validators, FormValidators} from './validators';
-export {FormValidationContext} from "./form-validation";
-export {FormValidationData} from "./form-validation";
